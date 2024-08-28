61ba65d6ce79c928fcafe20a1af02642a9c2343f
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2024-08-28 22:34:25 +0700

    ok:

--- src/app/core/top-nav/top-nav.component.ts
+++ src/app/core/top-nav/top-nav.component.ts
@@ -3,8 +3,10 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-top-nav",
   templateUrl: "./top-nav.component.html",
-  styleUrl: "./top-nav.component.css"
+  styleUrl: "./top-nav.component.css",
+  host: {
+    class : "container-fluid ps-0"
+  }
 })
 export class TopNavComponent {
-
 }
