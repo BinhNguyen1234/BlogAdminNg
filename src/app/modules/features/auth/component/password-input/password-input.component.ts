f29644b48f200ce39eb49d9e5826b34ab285d99c
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2024-09-11 23:34:49 +0700

    ok

--- src/app/modules/features/auth/component/password-input/password-input.component.ts
+++ src/app/modules/features/auth/component/password-input/password-input.component.ts
@@ -3,8 +3,12 @@ import { Component, Input } from "@angular/core";
 @Component({
   selector: "app-password-input",
   templateUrl: "./password-input.component.html",
-  styleUrl: "./password-input.component.css"
+  styleUrl: "./password-input.component.css",
+  host: {
+    class : "position-relative"
+  }
 })
 export class PasswordInputComponent {
   @Input() useUnhidePassword = false;
+  isHide = true;
 }
--- src/app/modules/features/auth/component/unhide-password-btn/unhide-password-btn.component.ts
+++ src/app/modules/features/auth/component/unhide-password-btn/unhide-password-btn.component.ts
@@ -1,15 +1,20 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, Input, Output } from "@angular/core";
+import { Subject } from "rxjs";
 
 @Component({
   selector: "app-unhide-password-btn",
   templateUrl: "./unhide-password-btn.component.html",
-  styleUrl: "./unhide-password-btn.component.css"
+  styleUrl: "./unhide-password-btn.component.css",
+  host: {
+    class : "position-absolute end-0 top-50 translate-middle-y",
+    role: "button"
+  }
 })
 export class UnhidePasswordBtnComponent {
-  @Input() isPasswordHide = true;
-  @Output() isPasswordHideChange = new EventEmitter<boolean>();
-  HandleClick (_e:MouseEvent){
+  @Input() isPasswordHide = false;
+  @Output() isPasswordHideChange = new Subject<boolean>();
+  HandleClick (_e?:MouseEvent){
     this.isPasswordHide = !this.isPasswordHide;
-    this.isPasswordHideChange.emit(this.isPasswordHide);
+    this.isPasswordHideChange.next(this.isPasswordHide);
   }
 }
